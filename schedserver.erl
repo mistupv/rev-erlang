ce5790730b011c51dc62b45eca5a7b6fd91118c1
Adrian Palacios <ukonat@gmail.com>
2017-03-02 18:54:31 +0100

    Added uniform scheduler process

---
+++ schedserver.erl
@@ -0,0 +1,28 @@
+-module(schedserver).
+-export([start/0]).
+
+start() ->
+  loop().
+
+loop() ->
+  receive
+    {Pid,System} ->
+      RandPid = randPid(System),
+      Pid ! RandPid,
+      loop();
+    terminate -> ok
+  end.
+
+randPid({Gamma,Procs}) ->
+  case Gamma of
+    [] ->
+      Pids = [Pid || {Pid,_,_} <- Procs],
+      PidsLen = length(Pids),
+      RandPid = random:uniform(PidsLen);
+    _Other ->
+      Pids = [gamma] ++ [Pid || {Pid,_,_} <- Procs],
+      GammaPidsLen = length(Pids),
+      RandPid = random:uniform(GammaPidsLen)
+  end,
+  lists:nth(RandPid,Pids).
+  
