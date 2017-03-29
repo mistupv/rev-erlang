688e694c3179e9ca65939e00b7adee08292d8944
Baha <ukonat@gmail.com>
2017-03-29 11:33:28 +0200

    Improved select_proc comprehension lists

--- utils.erl
+++ utils.erl
@@ -2,6 +2,7 @@
 -export([select_proc/2]).
 
 select_proc(Procs,Pid) ->
-  [Proc] = [{P,S,M} || {P,S,M} <- Procs, P == Pid],
-  RestProcs = [{P,S,M} || {P,S,M} <- Procs, P /= Pid],
+  [Proc] = [Item || Item = {P,_,_} <- Procs, P == Pid],
+  RestProcs = [Item || Item = {P,_,_} <- Procs, P /= Pid],
   {Proc,RestProcs}.
+
