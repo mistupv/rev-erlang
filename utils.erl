9036cfb2c8b24ff3ed83da8f0d2a8d07df95bbfb
Adrian Palacios <ukonat@gmail.com>
2017-04-04 17:50:17 +0200

    zip_core and top prettyprint calls

--- utils.erl
+++ utils.erl
@@ -1,8 +1,26 @@
 -module(utils).
--export([select_proc/2]).
+-export([select_proc/2,zip_core/2,pp_system/1]).
 
 select_proc(Procs,Pid) ->
   [Proc] = [Item || Item = {P,_,_,_} <- Procs, P == Pid],
   RestProcs = [Item || Item = {P,_,_,_} <- Procs, P /= Pid],
   {Proc,RestProcs}.
 
+zip_core([],{c_literal,_,[]}) ->
+  [];
+zip_core([E|Es],{c_cons,_Ann,Head,Rest}) ->
+  [{E,Head}|zip_core(Es,Rest)].
+
+pp_system({Gamma,Procs}) ->
+  pp_gamma(Gamma) ++ ";" ++ pp_procs(Procs).
+
+pp_gamma([]) -> "[]";
+pp_gamma(Gamma) -> "[" ++ string:join([pp_msg(X) || X <- Gamma],",") ++ "]".
+
+pp_procs(Procs) -> "".
+
+pp_msg({SrcPid,DestPid,MsgValue}) ->
+  "{" ++ pp(SrcPid) ++ "," ++ pp(DestPid) ++ "," ++ pp(MsgValue) ++ "}".
+
+% TODO: Ask Tama about this
+pp(X) -> "".%erl_prettypr:format(X).
