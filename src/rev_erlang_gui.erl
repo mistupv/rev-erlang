894c67a0e7ecf68b2959b4dd62b3f622877e9112
Baha <ukonat@gmail.com>
2017-05-06 12:04:44 +0200

    Buttons for rules (random and specific)

--- src/rev_erlang_gui.erl
+++ src/rev_erlang_gui.erl
@@ -31,7 +31,7 @@ setup_gui() ->
 setupLeftColumnSizer(Parent) ->
   StateText = wxTextCtrl:new(Parent, 1001,
                              [{style,?wxTE_MULTILINE bor ?wxTE_READONLY},
-                              {size,{460,260}}]),
+                              {size,{460,460}}]),
   InputStaticText = wxStaticText:new(Parent, ?wxID_ANY, "Input args:"),
   InputTextCtrl = wxTextCtrl:new(Parent, 1000,
                                  [{style, ?wxBOTTOM}]),
@@ -59,13 +59,19 @@ setupRightColumnSizer(Parent) ->
   BackwardCheckBox = wxCheckBox:new(Parent,?wxID_ANY,"Show backward rules"),
   wxCheckBox:setValue(ForwardCheckBox,true),
   wxCheckBox:setValue(BackwardCheckBox,true),
-  ButtonBackward = wxButton:new(Parent, ?ID_BACKWARD_STEP, [{label,"backward"}]),
-  ButtonForward = wxButton:new(Parent, ?ID_FORWARD_STEP, [{label,"forward"}]),
+
+  RandButton = wxButton:new(Parent, ?RAND_BUTTON, [{label,getButtonLabel(?RAND_BUTTON)}]),
+  ForwRandButton = wxButton:new(Parent, ?FORW_RAND_BUTTON, [{label,getButtonLabel(?FORW_RAND_BUTTON)}]),
+  BackRandButton = wxButton:new(Parent, ?BACK_RAND_BUTTON, [{label,getButtonLabel(?BACK_RAND_BUTTON)}]),
+
+  ForwardButtons = setupRuleButtons(Parent, ?FORW_SEQ_BUTTON, ?FORW_SCHED_BUTTON),
+  BackwardButtons = setupRuleButtons(Parent,?BACK_SEQ_BUTTON, ?BACK_SCHED_BUTTON),
 
   CtrlsSizer = wxBoxSizer:new(?wxVERTICAL),
   ProcInfoSizer = wxBoxSizer:new(?wxHORIZONTAL),
   RuleInfoSizer = wxBoxSizer:new(?wxVERTICAL),
-  RuleButtonSizer = wxBoxSizer:new(?wxHORIZONTAL),
+  RuleButtonSizer = wxBoxSizer:new(?wxVERTICAL),
+  RandButtonSizer = wxBoxSizer:new(?wxHORIZONTAL),
 
   wxSizer:addSpacer(CtrlsSizer,25),
   wxSizer:add(CtrlsSizer, ProcInfoSizer),
@@ -80,9 +86,59 @@ setupRightColumnSizer(Parent) ->
   wxSizer:add(RuleInfoSizer,ForwardCheckBox),
   wxSizer:add(RuleInfoSizer,BackwardCheckBox),
   
-  wxSizer:add(RuleButtonSizer, ButtonForward),
-  wxSizer:add(RuleButtonSizer, ButtonBackward),
+  wxSizer:add(RuleButtonSizer,RandButtonSizer),
+  wxSizer:add(RandButtonSizer,RandButton),
+  wxSizer:add(RandButtonSizer,ForwRandButton),
+  wxSizer:add(RandButtonSizer,BackRandButton),
+  wxSizer:addSpacer(RuleButtonSizer,15),
+  addButtonsToSizer(RuleButtonSizer,ForwardButtons),
+  wxSizer:addSpacer(RuleButtonSizer,15),
+  addButtonsToSizer(RuleButtonSizer,BackwardButtons),
+  % wxSizer:add(RuleButtonSizer, ButtonForward),
+  % wxSizer:add(RuleButtonSizer, ButtonBackward),
   CtrlsSizer.
+
+addButtonsToSizer(Sizer,Buttons) ->
+  FirstRowSizer = wxBoxSizer:new(?wxHORIZONTAL),
+  SecondRowSizer = wxBoxSizer:new(?wxHORIZONTAL),
+  LastRowSizer = wxBoxSizer:new(?wxHORIZONTAL),
+
+  FirstRowButtons = lists:sublist(Buttons,1,3),
+  SecondRowButtons = lists:sublist(Buttons,4,3),
+  LastRowButtons = lists:sublist(Buttons,7,3),
+
+  [wxSizer:add(FirstRowSizer,Button) || Button <- FirstRowButtons],
+  [wxSizer:add(SecondRowSizer,Button) || Button <- SecondRowButtons],
+  [wxSizer:add(LastRowSizer,Button) || Button <- LastRowButtons],
+
+  wxSizer:add(Sizer,FirstRowSizer),
+  wxSizer:add(Sizer,SecondRowSizer),
+  wxSizer:add(Sizer,LastRowSizer).
+
+getButtonLabel(Button) ->
+  case Button of
+    ?FORW_SEQ_BUTTON -> "Seq";
+    ?FORW_CHECK_BUTTON -> "Check";
+    ?FORW_SEND_BUTTON -> "Send";
+    ?FORW_RECEIVE_BUTTON -> "Receive";
+    ?FORW_SPAWN_BUTTON -> "Spawn";
+    ?FORW_SELF_BUTTON -> "Self";
+    ?FORW_SCHED_BUTTON -> "Sched";
+    ?BACK_SEQ_BUTTON -> "Seq";
+    ?BACK_CHECK_BUTTON -> "Check";
+    ?BACK_SEND_BUTTON -> "Send";
+    ?BACK_RECEIVE_BUTTON -> "Receive";
+    ?BACK_SPAWN_BUTTON -> "Spawn";
+    ?BACK_SELF_BUTTON -> "Self";
+    ?BACK_SCHED_BUTTON -> "Sched";
+    ?RAND_BUTTON -> "Random";
+    ?FORW_RAND_BUTTON -> "Forward";
+    ?BACK_RAND_BUTTON -> "Backward"
+  end.
+
+setupRuleButtons(Parent,First,Last) ->
+  [wxButton:new(Parent, Button,
+                [{label,getButtonLabel(Button)}]) || Button <- lists:seq(First,Last)].
 
 setupMenu() ->
   MenuBar = wxMenuBar:new(),
