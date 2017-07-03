a11f01abc7e8d4c5324b410e6f857b9cd051af75
Adrian Palacios <ukonat@gmail.com>
2017-07-03 11:53:25 +0200

    Updated codes for GUI widgets

--- src/rev_erlang_gui.hrl
+++ src/rev_erlang_gui.hrl
@@ -4,43 +4,43 @@
 -define(FRAME_SIZE_INIT, {800, 600}).
 
 -define(ABOUT, ?wxID_ABOUT).
--define(EXIT, ?wxID_EXIT).
--define(OPEN, ?wxID_OPEN).
+-define(EXIT,  ?wxID_EXIT).
+-define(OPEN,  ?wxID_OPEN).
 
--define(FORW_SEQ_BUTTON, 401).
--define(FORW_CHECK_BUTTON, 402).
--define(FORW_SEND_BUTTON, 403).
+-define(START_BUTTON,        400).
+-define(FORW_SEQ_BUTTON,     401).
+-define(FORW_CHECK_BUTTON,   402).
+-define(FORW_SEND_BUTTON,    403).
 -define(FORW_RECEIVE_BUTTON, 404).
--define(FORW_SPAWN_BUTTON, 405).
--define(FORW_SELF_BUTTON, 406).
--define(FORW_SCHED_BUTTON, 407).
--define(BACK_SEQ_BUTTON, 408).
--define(BACK_CHECK_BUTTON, 409).
--define(BACK_SEND_BUTTON, 410).
+-define(FORW_SPAWN_BUTTON,   405).
+-define(FORW_SELF_BUTTON,    406).
+-define(FORW_SCHED_BUTTON,   407).
+-define(BACK_SEQ_BUTTON,     408).
+-define(BACK_CHECK_BUTTON,   409).
+-define(BACK_SEND_BUTTON,    410).
 -define(BACK_RECEIVE_BUTTON, 411).
--define(BACK_SPAWN_BUTTON, 412).
--define(BACK_SELF_BUTTON, 413).
--define(BACK_SCHED_BUTTON, 414).
--define(FORWARD_BUTTON, 422).
--define(BACKWARD_BUTTON, 423).
--define(NORMALIZE_BUTTON, 424).
+-define(BACK_SPAWN_BUTTON,   412).
+-define(BACK_SELF_BUTTON,    413).
+-define(BACK_SCHED_BUTTON,   414).
+-define(FORWARD_BUTTON,      422).
+-define(BACKWARD_BUTTON,     423).
+-define(NORMALIZE_BUTTON,    424).
 
--define(SYSTEM,500).
--define(STATUS,501).
--define(FRAME, 509).
--define(INPUT_TEXT,502).
--define(PID_TEXT,507).
--define(STEP_TEXT,506).
--define(STATE_TEXT,510).
--define(CODE_TEXT,512).
--define(STATUS_BAR, 529).
--define(START_BUTTON,400).
--define(FUN_CHOICE,511).
--define(LEFT_NOTEBOOK,520).
--define(RIGHT_NOTEBOOK,521).
+-define(SYSTEM,         500).
+-define(STATUS,         501).
+-define(FRAME,          502).
+-define(INPUT_TEXT,     510).
+-define(PID_TEXT,       511).
+-define(STEP_TEXT,      512).
+-define(STATE_TEXT,     513).
+-define(CODE_TEXT,      514).
+-define(STATUS_BAR,     520).
+-define(FUN_CHOICE,     530).
+-define(LEFT_NOTEBOOK,  540).
+-define(RIGHT_NOTEBOOK, 541).
 
--define(PAGEPOS_CODE,0).
--define(PAGEPOS_STATE,1).
--define(PAGEPOS_MANU,0).
--define(PAGEPOS_SEMI,1).
--define(PAGEPOS_AUTO,2).
+-define(PAGEPOS_CODE,  0).
+-define(PAGEPOS_STATE, 1).
+-define(PAGEPOS_MANU,  0).
+-define(PAGEPOS_SEMI,  1).
+-define(PAGEPOS_AUTO,  2).
