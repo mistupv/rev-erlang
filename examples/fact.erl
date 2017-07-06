974898ef77d015344b237062897a39a023649634
Adrian Palacios <ukonat@gmail.com>
2017-07-06 18:39:52 +0200

    Factorial example with integers

--- examples/fact.erl
+++ examples/fact.erl
@@ -1,20 +1,5 @@
 -module(fact).
 -export([factorial/1]).
 
-factorial(X) ->
-  case X of
-    z -> {s,z};
-    {s,Y} -> mult(X, factorial(Y))
-  end.
-
-mult(X,Y) ->
-  case X of
-    z -> z;
-    {s,Z} -> sum(Y, mult(Z,Y))
-  end.
-
-sum(X,Y) ->
-  case X of
-    z -> Y;
-    {s,Z} -> W = sum(Z, Y), {s,W}
-  end.
+factorial(0) -> 1;
+factorial(N) -> N * factorial(N-1).
